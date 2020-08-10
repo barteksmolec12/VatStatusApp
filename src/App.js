b51a94089f9029db1854cd10978c32d584d2ebd0
barteksmolec12 <barteksmolec@o2.pl>
2020-08-10 22:30:28 +0200

    consume API

--- src/App.js
+++ src/App.js
@@ -36,25 +36,53 @@ class App extends React.Component
       },
       "requestId": "l7mc0-88dh943",
       "requestDateTime": "10-08-2020 18:49:39"
-    }}
+    }},
+    nip:"tes",
+    status:""
+    
 
   }
-  componentDidMount() {
+  getVatStatus() {
+    console.log("pobrano")
+ let nipFromInput=this.state.nip
+ let path="https://wl-api.mf.gov.pl/api/search/nip/"
+  path=path+nipFromInput+"?date=2020-05-05"
+  console.log(path)
 
-
-      fetch("https://wl-api.mf.gov.pl/api/search/nip/5511103933?date=2020-05-05")
+      fetch (path)
       .then(data =>data.json())
-      .then(json => this.setState({nips:json}));
+      .then(json => this.setState({status:json.result.subject.name}));
 
 
 
   }
+handleChange = (e)=> {
+  this.setState({nip:e.target.value})
+    console.log(e.target.value);
+  }
+  
 
 
 
   render()
   {
-    return(<h2>{this.state.nips.result.subject.name}</h2>)
+   
+  // this.handleChange = this.handleChange.bind(this);
+   this.getVatStatus = this.getVatStatus.bind(this);
+    return(
+      <div>
+              
+              <label>
+                Imię:
+                <input name="firstName" onChange={this.handleChange} />
+              </label>
+              <button onClick={this.getVatStatus}> Wyślij </button>
+
+                <h2>{this.state.status}</h2>
+                
+    </div>
+    
+    )
   }
 }
 export default App;
