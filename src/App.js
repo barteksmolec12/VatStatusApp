2032248d020ea9ff7a18ef5cf41e658074bbc390
barteksmolec12 <barteksmolec@o2.pl>
2020-08-10 19:39:21 +0200

    test api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+class App extends React.Component
+{
+  state={
+    nips:{"result": {
+      "subject": {
+        "name": "ZBIGNIEW SMOLEC",
+        "nip": "5511103933",
+        "statusVat": "Czynny",
+        "regon": "121130720",
+        "pesel": null,
+        "krs": null,
+        "residenceAddress": "BIADASOWSKA 9, 34-120 ZAGÓRNIK",
+        "workingAddress": null,
+        "representatives": [
+          
+        ],
+        "authorizedClerks": [
+          
+        ],
+        "partners": [
+          
+        ],
+        "registrationLegalDate": "2010-06-01",
+        "registrationDenialBasis": null,
+        "registrationDenialDate": null,
+        "restorationBasis": null,
+        "restorationDate": null,
+        "removalBasis": null,
+        "removalDate": null,
+        "accountNumbers": [
+          "81811000002001004435040001"
+        ],
+        "hasVirtualAccounts": false
+      },
+      "requestId": "l7mc0-88dh943",
+      "requestDateTime": "10-08-2020 18:49:39"
+    }}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test 3 <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  }
+  componentDidMount() {
+
+
+      fetch("https://wl-api.mf.gov.pl/api/search/nip/5511103933?date=2020-05-05")
+      .then(data =>data.json())
+      .then(json => this.setState({nips:json}));
+
+
+
+  }
+
+
+
+  render()
+  {
+    return(<h2>{this.state.nips.result.subject.name}</h2>)
+  }
 }
-
 export default App;
